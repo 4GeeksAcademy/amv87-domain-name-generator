f3f3b5a56be65097ced08a78a59bb2d4cdc37c01
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-10-09 19:37:36 +0000

    added function

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      let createDomain = pronoun[i] + adj[j] + noun[k] + ".com";
+      console.log(createDomain);
+    }
+  }
+}
