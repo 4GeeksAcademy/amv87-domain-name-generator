d528911f5252fa518596efa07c2cedf37b317ca2
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-10-10 19:46:57 +0000

    Added extensions

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let extension = [".com", ".net"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-      let createDomain = pronoun[i] + adj[j] + noun[k] + ".com";
-      console.log(createDomain);
+      for (let l = 0; l < extension.length; l++) {
+        let createDomain = pronoun[i] + adj[j] + noun[k] + extension[l];
+        console.log(createDomain);
+      }
     }
   }
 }
